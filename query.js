e3b8f48ea0bc373c85c1d99ad7596a3a3cc57b3a
pannang <patrickannang23@gmail.com>
2020-07-20 13:35:17 +0000

    Add files via upload

---
+++ query.js
@@ -0,0 +1,10 @@
+//navigation bar
+var navList = document.getElementById("nav_menu");
+
+function Show() {
+    navList.classList.add("_Menu-show");
+}
+
+function Hide() {
+    navList.classList.remove("_Menu-show");
+}
